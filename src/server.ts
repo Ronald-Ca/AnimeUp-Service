4239a95f8df67999b249d7bec42503fe6e896f89
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-03-25 20:49:56 -0400

    modificando

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import routes from './routes';
 const express = require('express');
-const readme = require('readmeio');
 
 dotenv.config();
 
@@ -14,18 +13,6 @@ mongoose.connect(process.env.DATABASE_URL as string).then(() => {
 
 const app = express();
 const PORT = process.env.PORT || 1818;
-
-app.use((req, res, next) => {
-    readme.log(process.env.README_KEY, req, res, {
-        // User's API Key
-        apiKey: 'owlbert-api-key',
-        // Username to show in ReadMe's dashboard
-        label: 'Owlbert',
-        // User's email address
-        email: 'owlbert@example.com',
-    });
-    return next();
-});
 
 app.use(express.json());
 app.use(routes);
