7d109f0c552a616caa7f02b8c9aecb1e196fa191
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-04-07 00:14:05 -0400

    organizando cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import routes from './routes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +14,14 @@ mongoose.connect(process.env.DATABASE_URL as string).then(() => {
 
 const app = express();
 const PORT = process.env.PORT || 1818;
+
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+))
 
 app.use(express.json());
 app.use(routes);
