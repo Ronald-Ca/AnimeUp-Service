2a9145288cb22251d6d17cbb12125ae9c7e85289
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-04-10 15:27:50 -0400

    novas atualizações

--- src/controllers/animeController.ts
+++ src/controllers/animeController.ts
@@ -38,6 +38,8 @@ export default class AnimeController {
                 trailer
             }
 
+            console.log(anime)
+
             const resService = await this._animeService.createAnime(anime)
 
             return res.status(200).json(responseSuccess('Success', resService));
